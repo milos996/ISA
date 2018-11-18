ed85ad4485a91cdea9ee120654885a94bd9030b8
Milos <ivankovicmilos5@gmail.com>
2018-11-18 12:54:00 +0100

    Add server structure

---
+++ server/src/main/java/com/example/ISAums/IsAumsApplication.java
@@ -0,0 +1,12 @@
+package com.example.ISAums;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IsAumsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IsAumsApplication.class, args);
+	}
+}
