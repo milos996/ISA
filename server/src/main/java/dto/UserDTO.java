c79187adc299fef89c9f0b99b6928d4691ad7e48
Uros Kojovic <uroskojo96@gmail.com>
2018-11-30 18:19:59 +0100

    Sredjivanje model klasa user; dodavanje paket za data transfer object

---
+++ server/src/main/java/dto/UserDTO.java
@@ -0,0 +1,48 @@
+package dto;
+
+import model.User;
+
+public class UserDTO {
+
+	private String firstName;
+	private String lastName;
+	private String email;
+	private String city;
+	private String telephoneNum;
+
+	public UserDTO() {}
+	
+	public UserDTO(String firstName, String lastName, String email, String city, String telephoneNum) {
+		super();
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.email = email;
+		this.city = city;
+		this.telephoneNum = telephoneNum;
+	}
+
+	public UserDTO(User user)
+	{
+		this(user.getFirstName(), user.getLastName(), user.getEmail(), user.getCity(), user.getTelephoneNum());
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public String getTelephoneNum() {
+		return telephoneNum;
+	}
+}
--- server/src/main/java/service/UserService.java
+++ server/src/main/java/service/UserService.java
@@ -3,12 +3,21 @@ package service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import model.User;
 import repository.UserRepository;
-
 
 @Service
 public class UserService {
 
 	@Autowired
 	private UserRepository userRepository;
+	
+	
+	public User save(User user) {
+		return userRepository.save(user);
+	}
+
+	public void remove(Long id) {
+		userRepository.deleteById(id);
+	}
 }
