ed9181a4f5fb9412078d66c75009e140830e88e7
Milos <ivankovicmilos5@gmail.com>
2018-12-15 08:05:13 +0100

    Add some dependencys

---
+++ client/src/components/Home.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class HomeComponent extends Component {
+  render() {
+    return (
+      <div>
+        <p>This is react Home component</p>
+      </div>
+    );
+  }
+}
+export default HomeComponent;
---
+++ client/src/components/Login.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class LoginComponent extends Component {
+  render() {
+    return (
+      <div>
+        <p>This is login component</p>
+      </div>
+    );
+  }
+}
+
+export default LoginComponent;
